f4a2b2f97ec5d63275051e4bc7e45cfecc99fb0b
Samuel Ariza Solano <samusus957@gmail.com>
2026-05-10 20:01:27 +0200

    Modificacion visual del buscador de propiedades

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './hero.component.html'
 })
-export class HeroComponent {}
+export class HeroComponent {
+  maxPrice: number = 500000;
+}
