74491f2809732120b2e271eed4d5ffae85477bfb
Samuel Ariza Solano <samusus957@gmail.com>
2026-05-14 17:57:35 +0200

    Le quitamos las cabeceras y pie de pagina solamente al login y al registro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -12,8 +13,22 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   isChatOpen = false;
+  showHeaderFooter = true;
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // Rutas donde NO queremos que aparezca el Header y el Footer
+      const hiddenRoutes = ['/login', '/registro'];
+      // Comprobamos si la url actual está en la lista (ignorando parámetros como ?id=1)
+      const currentUrl = event.urlAfterRedirects.split('?')[0];
+      this.showHeaderFooter = !hiddenRoutes.includes(currentUrl);
+    });
+  }
 
   toggleChat() {
     this.isChatOpen = !this.isChatOpen;
